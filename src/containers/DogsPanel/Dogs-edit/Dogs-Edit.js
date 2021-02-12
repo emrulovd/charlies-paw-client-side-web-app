8f53e11a9fc9d70efb393a9584af52966aa6025b
emrulovd <denisemrulov998@gmail.com>
2021-02-12 12:11:20 +0000

    Modifications 12/02/2021: Adding dynamically created inputs based on JS

--- src/containers/DogsPanel/Dogs-edit/Dogs-Edit.js
+++ src/containers/DogsPanel/Dogs-edit/Dogs-Edit.js
@@ -6,7 +6,7 @@ class DogsEdit extends Component{
     constructor(props){
         super(props);
         this.state = {
-            dog: {
+            dogForm: {
                 dog_name:{
                     elementType: 'input',
                     elementConfig: {
@@ -67,35 +67,31 @@ class DogsEdit extends Component{
         }
     }
 
+    inputChangeHandler = (event) => {
+        console.log(event.target.value);
+    }
+
     render(){
+        const formElementsArray = [];
+        for(let key in this.state.dogForm){
+            formElementsArray.push({
+                id: key,
+                config: this.state.dogForm[key]
+            })
+        }
         return(
             <div>
                 <Form>
-                    <Form.Group>
-                        <Form.Label>Dog's Name</Form.Label>
-                        <Form.Control type="text" placeholder="Enter dog's name" />
-                    </Form.Group>
-    
-                    <Form.Group>
-                        <Form.Label>Age of the dog</Form.Label>
-                        <Form.Control type="text" placeholder="Enter dog's age" />
-                    </Form.Group>
-                    <Form.Group>
-                        <Form.Label>Breed of the dog</Form.Label    >
-                        <Form.Control type="text" placeholder="Enter dog's breed" />
-                    </Form.Group>
-                    <Form.Group>
-                        <Form.Label>Breed's size</Form.Label    >
-                        <Form.Control type="text" placeholder="Enter breed's size" />
-                    </Form.Group>
-                    <Form.Group>
-                        <Form.Label>Location of the dog</Form.Label    >
-                        <Form.Control type="text" placeholder="Enter dog's location" />
-                    </Form.Group>
-                    <Form.Group>
-                    <Form.Label>Short discription of the</Form.Label>
-                        <Form.Control as="textarea" rows={3} />
-                    </Form.Group> 
+                    {formElementsArray.map((formElement) => (
+                        <Form.Group key = {formElement.id}>
+                            <Form.Label>{formElement.config.elementConfig.placeholder}</Form.Label>
+                            <Form.Control 
+                                elementype={formElement.config.elementType}
+                                elementconfig={formElement.config.elementConfig}
+                                value = {formElement.value}
+                                onChange={this.inputChangeHandler} />
+                        </Form.Group>
+                    ))}
                     <Button variant="primary" type="submit">
                         Submit
                     </Button>
