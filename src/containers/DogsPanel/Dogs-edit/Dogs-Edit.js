2c2fa6c520afe59eb1081d45466ffd09efd6c6a7
emrulovd <denisemrulov998@gmail.com>
2021-02-12 11:43:37 +0000

    Modifications 12/02/2021: Adding dog-edit page element configuration

--- src/containers/DogsPanel/Dogs-edit/Dogs-Edit.js
+++ src/containers/DogsPanel/Dogs-edit/Dogs-Edit.js
@@ -3,22 +3,99 @@ import React, { Component } from 'react';
 import { Form, Button } from 'react-bootstrap';
 
 class DogsEdit extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            dog: {
+                dog_name:{
+                    elementType: 'input',
+                    elementConfig: {
+                        type: 'text',
+                        placeholder: "Dog's name"
+                    },
+                    value: ''
+                },
+                age:{
+                    elementType: 'input',
+                    elementConfig: {
+                        type: 'text',
+                        placeholder: "Dog's age"
+                    },
+                    value: ''
+                },
+                breed:{
+                    elementType: 'input',
+                    elementConfig: {
+                        type: 'text',
+                        placeholder: "Dog's breed"
+                    },
+                    value: ''
+                },
+                breed_size:{
+                    elementType: 'input',
+                    elementConfig: {
+                        type: 'text',
+                        placeholder: "Breed's size"
+                    },
+                    value: ''
+                },
+                location:{
+                    elementType: 'input',
+                    elementConfig: {
+                        type: 'text',
+                        placeholder: "Dog's location"
+                    },
+                    value: ''
+                },
+                image:{
+                    elementType: 'input',
+                    elementConfig: {
+                        type: 'text',
+                        placeholder: "Dog's image"
+                    },
+                    value: ''
+                },
+                discription:{
+                    elementType: 'input',
+                    elementConfig: {
+                        type: 'textarea',
+                        placeholder: "Short discription about the dog"
+                    },
+                    value: ''
+                },
+            }
+        }
+    }
+
     render(){
         return(
             <div>
                 <Form>
-                    <Form.Group controlId="formBasicEmail">
-                        <Form.Label>Email address</Form.Label>
-                        <Form.Control type="email" placeholder="Enter email" />
+                    <Form.Group>
+                        <Form.Label>Dog's Name</Form.Label>
+                        <Form.Control type="text" placeholder="Enter dog's name" />
                     </Form.Group>
     
-                    <Form.Group controlId="formBasicPassword">
-                        <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" placeholder="Password" />
+                    <Form.Group>
+                        <Form.Label>Age of the dog</Form.Label>
+                        <Form.Control type="text" placeholder="Enter dog's age" />
                     </Form.Group>
-                    <Form.Group controlId="formBasicCheckbox">
-                        <Form.Check type="checkbox" label="Check me out" />
+                    <Form.Group>
+                        <Form.Label>Breed of the dog</Form.Label    >
+                        <Form.Control type="text" placeholder="Enter dog's breed" />
                     </Form.Group>
+                    <Form.Group>
+                        <Form.Label>Breed's size</Form.Label    >
+                        <Form.Control type="text" placeholder="Enter breed's size" />
+                    </Form.Group>
+                    <Form.Group>
+                        <Form.Label>Location of the dog</Form.Label    >
+                        <Form.Control type="text" placeholder="Enter dog's location" />
+                    </Form.Group>
+                    <Form.Group>
+                    <Form.Label>Short discription of the</Form.Label>
+                        <Form.Control as="textarea" rows={3} />
+                    </Form.Group> 
                     <Button variant="primary" type="submit">
                         Submit
                     </Button>
