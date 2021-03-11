68c46b992502f1c2ba5a846c3c7d04c45a340b42
emrulovd <denisemrulov998@gmail.com>
2021-03-11 13:38:09 +0000

    Modifications 11/03/2021: Adding the sidedrawer

--- src/components/Navigation/Toolbars.js
+++ src/components/Navigation/Toolbars.js
@@ -24,7 +24,7 @@ const Toolbar = (props) => {
             <nav>
                 <NavGroupMainItems/>
             </nav>
-            <Logo/>
+            <Logo className={classes.Logo}/>
             <nav>
                 <NavGroupDynamicItems isAuth={props.isAuth}/>
             </nav>
