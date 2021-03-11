98ba24ebce88292b9f2ddb54b0581675dca1d329
emrulovd <denisemrulov998@gmail.com>
2021-03-11 12:03:24 +0000

    Modifications 11/03/2021: Adding the about page Srtyling the about page

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,10 +1,29 @@
 import React from 'react';
 
+import {Container, Row, Col} from 'react-bootstrap';
+import AboutBanner from './AboutBanner/AboutBanner';
+import BatchTape from './BatchTape/BatchTape';
+import AboutLocations from './AboutLocations/AboutLocations';
+import AboutInfo from './AboutInfo/AboutInfo';
 
 const about = () =>{
     return(
         <div>
-            <h1>About</h1>
+            <AboutBanner/>
+            
+            <Container fluid>
+                <Row>
+                    <BatchTape/>
+                </Row>
+                <Row>
+                    <Col>
+                        <AboutLocations/>
+                    </Col>
+                    <Col>
+                        <AboutInfo/>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     )
 }
---
+++ src/components/About/BatchTape/BatchTape.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+import classes from './BatchTape.module.css';
+
+const batchTape = () => <div className={classes.Container}/> 
+
+
+export default batchTape; 
